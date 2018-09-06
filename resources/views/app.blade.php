93b702aae1e534dd719fb11eb9b3a94da62cf12e
Christiano Oishi <oishi.chris@gmail.com>
2018-09-06 13:15:04 -0300

    updated
    updated app screen

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,144 +14,106 @@ Frameworks
 
 
 <v-container>
-    <div>
-        <span class='display-3 grey--text  text--darken-3'>Frameworks</span>
-    </div>
-    <v-layout row wrap>
-        <v-flex xs12 sm6 md4>
-            <v-container fill-height>
-                <v-card color="blue" class="white--text" height="100%">
-                    <v-layout justify-space-between column fill-height>
-                        <v-flex>
-                            <v-img src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-300.png" aspect-ratio="2.75"></v-img>
-                            <v-card-title primary-title>
-                                <div>
-                                    <h3 class="headline mb-0">Vuetify</h3>
-                                    <div>Tidelift gives software development teams a single source for purchasing and
-                                        maintaining their software, with professional-grade assurances from the experts
-                                        who
-                                        know it best, while seamlessly integrating with existing tools.</div>
-                                </div>
-                            </v-card-title>
-                        </v-flex>
-                        <v-flex style='height:100px'>
-                            <v-layout align-end row fill-height>
-                                <v-flex>
-                                    <v-divider></v-divider>
-                                    <v-card-actions>
-                                        <v-btn color="error" @click='linkopen("https://vuetifyjs.com/pt-BR/getting-started/quick-start")'>Documentation</v-btn>
-                                    </v-card-actions>
-                                </v-flex>
-                            </v-layout>
+    <section>
+        <h1 class="display-1 primary--text">
+            <div>
+                <div>
+                    <p>Frameworks</p>
+                </div>
+            </div>
+        </h1>
+        <v-card>
+            <v-layout row wrap>
 
-                        </v-flex>
-                    </v-layout>
-                </v-card>
-            </v-container>
+                <template v-for='frame in frameworks'>
+                    <v-flex xs12 sm6 md4>
+                        <v-container fill-height>
+                            <v-card :color="frame.color" :class="frame.colortext" height="100%">
+                                <v-layout justify-space-between column fill-height>
+                                    <v-flex>
+                                        <v-img :src="frame.img"
+                                            aspect-ratio="2.75"></v-img>
+                                        <v-card-title primary-title>
+                                            <div>
+                                                <h3 class="headline mb-0" v-html='frame.title'></h3>
+                                                <div v-html='frame.desc'></div>
+                                            </div>
+                                        </v-card-title>
+                                    </v-flex>
+                                    <v-flex style='height:100px'>
+                                        <v-layout align-end row fill-height>
+                                            <v-flex>
+                                                <v-divider></v-divider>
+                                                <v-card-actions>
+                                                    <v-btn color="error" @click='linkopen(frame.doc)'>Documentation</v-btn>
+                                                </v-card-actions>
+                                            </v-flex>
+                                        </v-layout>
 
+                                    </v-flex>
+                                </v-layout>
+                            </v-card>
+                        </v-container>
+                    </v-flex>
+                </template>
+            </v-layout>
+        </v-card>
 
-        </v-flex>
+    </section>
 
-        <v-flex xs12 sm6 md4>
-            <v-container fill-height>
-                <v-card height="100%">
-                    <v-layout justify-space-between column fill-height>
-                        <v-flex>
-                            <v-img src="https://cdn.freebiesupply.com/logos/large/2x/laravel-1-logo-png-transparent.png"
-                                aspect-ratio="2.75"></v-img>
-                            <v-card-title primary-title>
-                                <div>
-                                    <h3 class="headline mb-0">Laravel</h3>
-                                    <div>Laravel is free, open-source and one of the more popular PHP web framework
-                                        based
-                                        on the
-                                        model–view–controller (MVC) architectural pattern. It is created by Taylor
-                                        Otwell,
-                                        intended
-                                        to reduce the cost of initial development and improve quality of your code by
-                                        defining
-                                        industry standard design practices. Using Laravel, you can save hours of
-                                        development time
-                                        and reduce thousands of lines of code compared raw PHP.</div>
-                                </div>
-                            </v-card-title>
-                        </v-flex>
-                        <v-flex style='height:100px'>
-                            <v-layout align-end row fill-height>
-                                <v-flex>
-                                    <v-divider></v-divider>
-                                    <v-card-actions>
-                                        <v-btn color="error" @click='linkopen("https://laravel.com/docs/5.7")'>Documentation</v-btn>
-                                    </v-card-actions>
-                                </v-flex>
-                            </v-layout>
-
-                        </v-flex>
-                    </v-layout>
-                </v-card>
-            </v-container>
-        </v-flex>
-        <v-flex xs12 sm6 md4>
-            <v-container fill-height>
-                <v-card color="teal accent-4" class="white--text" height="100%">
-                    <v-layout justify-space-between column fill-height>
-                        <v-flex>
-                            <v-img src="https://dwglogo.com/wp-content/uploads/2017/09/Vue-logo-001.svg" aspect-ratio="2.75"></v-img>
-                            <v-card-title primary-title>
-                                <div>
-                                    <h3 class="headline mb-0">Vue.js</h3>
-                                    <div>Vue (pronounced /vjuː/, like view) is a progressive framework for building
-                                        user
-                                        interfaces. Unlike other monolithic frameworks, Vue is designed from the ground
-                                        up
-                                        to be incrementally adoptable. The core library is focused on the view layer
-                                        only,
-                                        and is easy to pick up and integrate with other libraries or existing projects.
-                                        On
-                                        the other hand, Vue is also perfectly capable of powering sophisticated
-                                        Single-Page
-                                        Applications when used in combination with modern tooling and supporting
-                                        libraries.</div>
-                                </div>
-                            </v-card-title>
-                        </v-flex>
-                        <v-flex style='height:100px'>
-                            <v-layout align-end row fill-height>
-                                <v-flex>
-                                    <v-divider></v-divider>
-                                    <v-card-actions>
-                                        <v-btn color="error" @click='linkopen("https://vuejs.org/v2/guide/")'>Documentation</v-btn>
-                                    </v-card-actions>
-                                </v-flex>
-                            </v-layout>
-
-                        </v-flex>
-                    </v-layout>
-                </v-card>
-            </v-container>
-        </v-flex>
-    </v-layout>
 </v-container>
 
-
-
+<!--
+###########
+Tips
+###########
+-->
 <v-divider></v-divider>
+<v-container>
+
+    <section>
+        <h1 class="display-1 primary--text">
+            <div>
+                <div>
+                    <p>Tips</p>
+                </div>
+            </div>
+        </h1>
+
+        <v-card>
+            <v-container>
+                <template v-for='tip in tips'>
+                    <div class='mt-1'>
+                        <v-card color='primary' class='white--text'>
+                            <v-container>
+                                <v-layout row>
+                                    <v-flex xs6 v-html='tip.desc'></v-flex>
+                                    <v-flex xs6 v-html='tip.using'></v-flex>
+                                </v-layout>
+                            </v-container>
+                        </v-card>
+                    </div>
+                </template>
+
+            </v-container>
+        </v-card>
+    </section>
+
+
+
+</v-container>
+
 
 <!--
 ###########
 Example
 ###########
 -->
-
-
-
+<v-divider></v-divider>
 <v-container>
-    <div><span class='display-3 grey--text  text--darken-3'>Examples - Using Vuetify Components</span></div>
+
+    <div><span class='display-2 grey--text  text--darken-3'>Examples - Using Vuetify Components</span></div>
 </v-container>
-
-
-
-
 
 @endsection
 
@@ -194,7 +156,6 @@ Footer
 </v-card>
 @endsection
 
-
 <!--
 ###########
 Vue
@@ -219,21 +180,41 @@ Vue
         },
         data: {
             drawer: null,
-            dialog: null,
-            items: [{
-                    src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+            tips: [{
+                    desc: "Vue variables on Blade",
+                    using: "Use @ before &#123;&#123; &#125;&#125;"
                 },
                 {
-                    src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
-                },
-                {
-                    src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
-                },
-                {
-                    src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+                    desc: "Vue variables on Blade",
+                    using: "Use @ before"
                 }
             ],
-
+            frameworks:[
+                {
+                    color: 'blue',
+                    colortext: 'white--text',
+                    img: "https://cdn.vuetifyjs.com/images/logos/vuetify-logo-300.png",
+                    title: 'Vuetify',
+                    desc: "Tidelift gives software development teams a single source for purchasing and maintaining their software, with professional-grade assurances from the experts who know it best, while seamlessly integrating with existing tools.",
+                    doc: "https://vuetifyjs.com/pt-BR/getting-started/quick-start"
+                },
+                {
+                    color: 'white',
+                    colortext: 'black--text',
+                    img: "https://cdn.freebiesupply.com/logos/large/2x/laravel-1-logo-png-transparent.png",
+                    title: 'Vue.js',
+                    desc: "Laravel is free, open-source and one of the more popular PHP web framework based on the model–view–controller (MVC) architectural pattern. It is created by Taylor Otwell, intended to reduce the cost of initial development and improve quality of your code by defining industry standard design practices. Using Laravel, you can save hours of development time and reduce thousands of lines of code compared raw PHP.",
+                    doc: "https://laravel.com/docs/5.7"
+                },
+                {
+                    color: 'teal accent-4',
+                    colortext: 'white--text',
+                    img: "https://dwglogo.com/wp-content/uploads/2017/09/Vue-logo-001.svg",
+                    title: 'Vue.js',
+                    desc: "Vue (pronounced /vjuː/, like view) is a progressive framework for building user interfaces. Unlike other monolithic frameworks, Vue is designed from the ground up to be incrementally adoptable. The core library is focused on the view layer only, and is easy to pick up and integrate with other libraries or existing projects. On the other hand, Vue is also perfectly capable of powering sophisticated Single-Page Applications when used in combination with modern tooling and supporting libraries.",
+                    doc: "https://vuejs.org/v2/guide/"
+                }
+            ]
         },
         methods: {
             linkopen: function (link) {
@@ -243,6 +224,5 @@ Vue
 
 
     });
-
 </script>
 @endsection
